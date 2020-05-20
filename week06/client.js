0b153378b3540eb5267b52051e0337c4c2a2ac77
huzhenggen <huzhenggen@qxintech.com>
2020-05-20 23:25:05 +0800

    :sparkles: 第六周作业

--- week06/client.js
+++ week06/client.js
@@ -217,7 +217,9 @@ void async function () {
     };
     let request = new Request(options);
     let response = await request.send();
-    console.log(response)
+    let dom = parser.parseHTML(response.body);
+    console.log(response);
+    console.log(dom);
 }();
 
 
